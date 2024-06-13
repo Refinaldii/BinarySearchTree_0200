a07edd2896877cf21521075057857537c5103b92
Refinaldii <refinaldi9786@gmail.com>
2024-06-13 10:57:29 +0700

    membuat prosedur untuk menambahkan nilai

--- BinarySearchTree_0200.cpp
+++ BinarySearchTree_0200.cpp
@@ -29,6 +29,19 @@ public:
 
     }
 
+    void insert(string element) //Insert a node in the binary search tree
+    {
+
+        Node* newNode = new Node(element, nullptr, nullptr); //Allocare memory for the new node
+        newNode->info = element; // Assign value the data field of the new node
+        newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; //make the right childe of the new node point to NULL
+
+
+
+
+    }
+
 };
 
 int main()
