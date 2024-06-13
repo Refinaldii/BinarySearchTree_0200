8c2ec73d4a2edcc0f22ea40a16657763327fedb5
Refinaldii <refinaldi9786@gmail.com>
2024-06-13 10:49:07 +0700

    Tambahkan konstruktor Node untuk inisialisasi info, leftchild, dan rightchild

--- BinarySearchTree_0200.cpp
+++ BinarySearchTree_0200.cpp
@@ -10,6 +10,12 @@ public:
     Node* rightchild;
 
     //constructor for the node class
+    Node(string i, Node* l, Node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
 
 int main()
