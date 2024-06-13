f6c11efa36e453f923683d0b0dce38644df9f9c7
Refinaldii <refinaldi9786@gmail.com>
2024-06-13 11:18:40 +0700

    membuat void search

--- BinarySearchTree_0200.cpp
+++ BinarySearchTree_0200.cpp
@@ -2,14 +2,13 @@
 #include <string>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     string info;
     Node* leftchild;
     Node* rightchild;
 
-    //constructor for the node class
+    //Constructor for the node class
     Node(string i, Node* l, Node* r)
     {
         info = i;
@@ -22,47 +21,54 @@ class BinaryTree
 {
 public:
     Node* ROOT;
-    
-    BinaryTree()
-    {
-        ROOT = nullptr; //initializing ROOT to null
 
+    BinaryTree() {
+        ROOT = nullptr; //Initializing ROOT to null
     }
 
-    void insert(string element) //Insert a node in the binary search tree
+    void insert(string element) // insert a node in the binary search tree
     {
-
-        Node* newNode = new Node(element, nullptr, nullptr); //Allocare memory for the new node
-        newNode->info = element; // Assign value the data field of the new node
-        newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
-        newNode->rightchild = nullptr; //make the right childe of the new node point to NULL
+        Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
+        newNode->info = element; //assign value to the data field of the new node
+        newNode->leftchild = nullptr; //make the left child of the new node point to null
+        newNode->rightchild = nullptr; //make the right child of the newnode point to null
 
         Node* parent = nullptr;
         Node* currentNode = nullptr;
-        search(element, parent, currentNode); //Locate the node which will be the parent of the node to be insert
+        search(element, parent, currentNode); // locate the node which will be the parent of the node to be insert
 
-        if (parent == nullptr) // if the parent null (tree is empty)
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
         {
-            ROOT = newNode; // Mark the new node as ROOT
-            return; //exit
+            ROOT = newNode; // mark the new node as root
+            return; // exit
         }
 
-        if (element < parent->info) // if the value in the data field of the new node is less than
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
         {
-            parent->leftchild = newNode;
+            parent->leftchild = newNode; //make the left child of the parent point to the new node
         }
-        else if (element > parent->info)
+        else if (element > parent->info) //if the value in the data field of the new node is greater than that of the parent
         {
-            parent->rightchild = newNode;
+            parent->rightchild = newNode; // make  the right child of the parent point to the new node
         }
-
-
     }
 
+    void search(string element, Node*& parent, Node*& currentNode) {
+        //this fuction searches the currentNode of the specified NOde as well as the currnet nOde of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 };
 
 int main()
 {
-    
-}
 
+}
